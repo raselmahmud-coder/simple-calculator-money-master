32238a896563ea26e0423a6af8855adfeee0bd0f
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-02-18 00:00:57 +0800

    error handling is done

--- js/app.js
+++ js/app.js
@@ -1,24 +1,46 @@
 // Calling all elements and keep store
-const income = getElementId('income');
-const food = getElementId('food');
-const rent = getElementId('rent');
-const clothe = getElementId('clothe');
+const calculateEventListen = getElementId('calculate');
+const saveEventListen = getElementId('save-button');
+const incomeInput = getElementId('income');
+const foodInput = getElementId('food');
+const rentInput = getElementId('rent');
+const clotheInput = getElementId('clothe');
 const totalExpenses = getElementId('total-expenses');
 const balance = getElementId('balance');
-const percentage = getElementId('percentage');
+const percentageInput = getElementId('percentage');
 const savingAmount = getElementId('saving-amount');
 const remainingBalance = getElementId('remaining-balance');
+const popupOne = getElementId("error-show-one");
+const popupTwo = getElementId("error-show-two");
 // Get Elements id using the function
 function getElementId(elementId) {
-   return document.getElementById(elementId);
+    return document.getElementById(elementId);
 }
+// Expenses Calculate Event Handler
+calculateEventListen.addEventListener('click', () => {
+    getConvertValue(foodInput.value, rentInput.value, clotheInput.value, incomeInput.value);
+});
+// Save Event Handler
+saveEventListen.addEventListener('click', () => {
+    getSaving(incomeInput.value, percentageInput.value);
+});
 /* Expenses area calculation function*/
 function getConvertValue(food, rent, clothe, income) {
     // validation error handling
     if (isNaN(parseFloat(food)) || isNaN(parseFloat(rent)) || isNaN(parseFloat(clothe))|| isNaN(parseFloat(income))) {
-        return alert(`It's not a Number ${food} ${rent} ${clothe} ${income}`);
-    }else if (parseFloat(food) < 0 || parseFloat(rent) < 0 || parseFloat(clothe) < 0 || parseFloat(income) < 0) {
-        return alert(`it's less the zero`);
+        return alert(`It's not a Number`);
+    }
+    else if (parseFloat(food) < 0) {
+        return alert(`Food's less the zero`);
+    }
+    else if (parseFloat(rent) < 0) {
+        return alert(`Rent's less the zero`);
+    }
+    else if (parseFloat(clothe) < 0) {
+        return alert(`Clothe's less the zero`);
+    }
+    else if (parseFloat(income) < 0) {
+        return alert(`Income's less the zero`);
     }
     // calculation process
     const totalCost = parseFloat(food) + parseFloat(rent) + parseFloat(clothe);
@@ -28,27 +50,27 @@ function getConvertValue(food, rent, clothe, income) {
         totalExpenses.innerText = totalCost;
         balance.innerText = parseFloat(income) - parseFloat(totalExpenses.innerText);
     }
+    foodInput.value = '';
+    rentInput.value = '';
+    clotheInput.value = '';
 }
-// Expenses Calculate Event Handler
-document.getElementById('calculate').addEventListener('click', () => {
-    getConvertValue(food.value, rent.value, clothe.value, income.value);
-});
 /* Saving area calculation function*/
 function getSaving(income, percentage) {
     if (isNaN(parseInt(income)) || isNaN(parseInt(percentage))) {
-        return alert(`it's not a number in the saving input`);
-    } else if (parseFloat(income) < 0 || parseFloat(percentage) < 0 || parseFloat(percentage) > 100) {
-        return alert(`It's less the zero percentage`);  
+        popupOne.classList.toggle("show");
+        setTimeout(() => popupOne.classList.remove("show"), 4000);
+        return;
+    } else if (parseFloat(income) < 0 || parseFloat(percentage) < 0) {
+        return alert(`It's less the zero percentage value`);  
     }
     const saveAmount = parseFloat(income) * (parseFloat(percentage) / 100);
     if (saveAmount > parseFloat(balance.innerText)) {
-        return alert(`Sorry! remaining balance is not enough`);
-    } else {
+        popupTwo.classList.toggle("show");
+        setTimeout(() => popupTwo.classList.remove("show"), 5000);
+        return;
+    }else {
         savingAmount.innerText = saveAmount;
         remainingBalance.innerText = parseFloat(balance.innerText) - saveAmount;
     }
+    percentageInput.value = '';
 }
-// Save Event Handler
-document.getElementById('save-action').addEventListener('click', () => {
-    getSaving(income.value, percentage.value);
-})
